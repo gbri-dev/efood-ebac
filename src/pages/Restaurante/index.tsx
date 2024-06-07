8c6ce931f8d4736c10f4beceb5a0f7ab2f8ba67a
gbri-dev <gabriel.camposdasilva@hotmail.com>
2024-06-07 08:40:47 -0300

    Feat(Modal): Detalhes do prato

--- src/pages/Restaurante/index.tsx
+++ src/pages/Restaurante/index.tsx
@@ -2,7 +2,6 @@ import Banner from '../../components/Restaurante/Banner'
 import Header from '../../components/Header'
 import Prato from '../../components/Restaurante/Prato'
 import { Detalhes } from './styles'
-import ModalAdd from '../../components/Restaurante/Modal'
 
 const RestauranteDetalhes = () => {
   return (
@@ -17,7 +16,6 @@ const RestauranteDetalhes = () => {
         <Prato />
         <Prato />
       </Detalhes>
-      <ModalAdd isOpenModal={false} />
     </>
   )
 }
